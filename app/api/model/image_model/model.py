ec2dce83d7f1e248e2a6500ebf5a12e89e29bdc7
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-02-26 21:32:54 +0530

    Integrated the image prediction model at route /api/model/predict

--- app/api/model/image_model/model.py
+++ app/api/model/image_model/model.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, request, send_from_directory
 import os
-
+from ultralytics import YOLO
+from PIL import Image
+import base64
+from io import BytesIO
 
 model_bp = Blueprint('model', __name__)
 
@@ -33,3 +36,42 @@ def upload_file():
 @model_bp.route('/image')
 def get_file():
     return send_from_directory('./uploads','uploaded_image.png')
+
+@model_bp.route('/predict')
+def predict():
+    filename = "uploaded_image.png" 
+
+    file_path = os.path.join("./app/uploads", filename)
+    if not os.path.exists(file_path):
+        return jsonify({"error": f"File not found: {filename}"}), 404
+
+    try:
+        model_path = os.path.join("./app/api/model/image_model", 'best.pt')
+        model = YOLO(model_path)
+        results = model(file_path)
+        result = results[0]
+        box = result.boxes[0]
+        class_id = int(box.cls[0].item())
+        
+        stage = "normal"
+        if class_id == 0:
+            stage = "bald"
+        elif class_id == 1:
+            stage = "normal"
+        elif class_id == 2:
+            stage = "stage 1"
+        elif class_id == 3:
+            stage = "stage 2"
+        elif class_id == 4:
+            stage = "stage 3"
+
+        im_array = result.plot()
+        result_image = Image.fromarray(im_array[..., ::-1])
+        image_buffer = BytesIO()
+        result_image.save(image_buffer, format="PNG")
+        image_data = base64.b64encode(image_buffer.getvalue()).decode("utf-8")        
+
+        return jsonify({"stage": f"{stage}", "file": image_data}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
